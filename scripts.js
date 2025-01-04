93e84cc3c6ad58d564f34e9fd70d8e1d21becc0d
Alan Pisani <alanpisani1993@gmail.com>
2025-01-03 22:55:58 -0300

    Rehaciendo el portfolio

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Ejemplo de scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
